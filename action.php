7e10717581e3daab4c0f3789f4b8403c7ab9fb1d
Amrit <35733279+amrit1996@users.noreply.github.com>
2019-03-14 20:52:36 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,15 @@
+<?php
+  if(isset($_POST["action "])){
+  	switch ($_POST["action "]) {
+  		case 'add':
+  			# code...
+  			break;
+  		
+  		default:
+  			# code...
+  			break;
+  	}
+  }else{
+  	header('location:abc.php');
+  }
+?>
